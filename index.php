d1b4e1faf95fbc1ee24cdd213544fc23fcfca3ab
Matt Graham <graham@github.com>
2013-01-01 19:22:44 -0500

    added facebook comments

--- index.php
+++ index.php
@@ -25,16 +25,7 @@
 						  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=252447711526668";
 						  fjs.parentNode.insertBefore(js, fjs);
 						}(document, 'script', 'facebook-jssdk'));</script>
-				</div>
-
-				<div id="section">
-					<h3>Headed to Chemo</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula congue scelerisque. Proin a quam a mi pretium mollis. Maecenas viverra tincidunt ipsum, non porta ipsum egestas ac. In ac ligula vel sem rhoncus lobortis. Quisque dignissim arcu non arcu tempor tincidunt. Aenean dui dui, tempus eu ultrices et, dignissim id libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut convallis scelerisque nunc, vel rutrum odio luctus et. Praesent sodales, leo eget dictum porttitor, nisi diam imperdiet metus, non lobortis odio eros lobortis arcu. Maecenas pulvinar ornare viverra. Pellentesque in malesuada velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla sollicitudin laoreet molestie. Etiam facilisis risus et velit placerat ornare. Integer ac neque ante.</p>
-				</div>
-
-				<div id="section">
-					<h3>Headed to Chemo</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula congue scelerisque. Proin a quam a mi pretium mollis. Maecenas viverra tincidunt ipsum, non porta ipsum egestas ac. In ac ligula vel sem rhoncus lobortis. Quisque dignissim arcu non arcu tempor tincidunt. Aenean dui dui, tempus eu ultrices et, dignissim id libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut convallis scelerisque nunc, vel rutrum odio luctus et. Praesent sodales, leo eget dictum porttitor, nisi diam imperdiet metus, non lobortis odio eros lobortis arcu. Maecenas pulvinar ornare viverra. Pellentesque in malesuada velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla sollicitudin laoreet molestie. Etiam facilisis risus et velit placerat ornare. Integer ac neque ante.</p>
+					<div class="fb-comments" data-href="http://madebygraham.com" data-width="460" data-num-posts="1"></div>
 				</div>
 
 			</div>
@@ -43,15 +34,16 @@
 				<h3>143 Supporters</h3>
 				<a href="#">View All Supporters</a>
 				<br/>
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
 			</div>
 		</div>
 	</body>
