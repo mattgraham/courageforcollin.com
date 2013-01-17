3b96640f2b3175e7f83873a01c9477e87630172d
Matt Graham <graham@github.com>
2013-01-16 22:08:46 -0800

    instagram

--- index.php
+++ index.php
@@ -1,50 +1,29 @@
-<html>
-<head>
-	<script type="text/javascript" src="//use.typekit.net/cuc6ksk.js"></script>
-	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-	<link rel="stylesheet" href="stylesheets/screen.css" />
-</head>
-	<body>
-		<header class="wrapper">
-			<h1>We've got your back Collin</h1>
-			<p>Our buddy Collin is battling cancer, so we're battling it with him. Let Collin and those fighting know you're praying and thinking of him, while getting updates on his progress.</p>
-			<div id="facebook" class="fb-button">Login w/facebook to join in</div>
-		</header>
-		<div class="wrapper">
-			<div id="updates">
+<?php
+    // Get class for Instagram
+    // More examples here: https://github.com/cosenary/Instagram-PHP-API
+    require_once 'instagram.class.php';
 
-				<div id="section">
-					<h3>Headed to Chemo</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula congue scelerisque. Proin a quam a mi pretium mollis. Maecenas viverra tincidunt ipsum, non porta ipsum egestas ac. In ac ligula vel sem rhoncus lobortis. Quisque dignissim arcu non arcu tempor tincidunt. Aenean dui dui, tempus eu ultrices et, dignissim id libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut convallis scelerisque nunc, vel rutrum odio luctus et. Praesent sodales, leo eget dictum porttitor, nisi diam imperdiet metus, non lobortis odio eros lobortis arcu. Maecenas pulvinar ornare viverra. Pellentesque in malesuada velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla sollicitudin laoreet molestie. Etiam facilisis risus et velit placerat ornare. Integer ac neque ante.</p>
-					<span class="quote">"This is going to be rough, but Collin is a fighter we're going to come out on the other side." - Matt Graham</span>
-					<div id="fb-root"></div>
-						<script>(function(d, s, id) {
-						  var js, fjs = d.getElementsByTagName(s)[0];
-						  if (d.getElementById(id)) return;
-						  js = d.createElement(s); js.id = id;
-						  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=252447711526668";
-						  fjs.parentNode.insertBefore(js, fjs);
-						}(document, 'script', 'facebook-jssdk'));</script>
-					<div class="fb-comments" data-href="http://madebygraham.com" data-width="460" data-num-posts="1"></div>
-				</div>
+    // Initialize class with client_id
+    // Register at http://instagram.com/developer/ and replace client_id with your own
+    $instagram = new Instagram('457f784dbf5b4580a4c256f84911cbc6');
 
-			</div>
+    // Set keyword for #hashtag
+    $tag = 'KEYWORD HERE';
 
-			<div id="supporters">
-				<h3>143 Supporters</h3>
-				<a href="#">View All Supporters</a>
-				<br/>
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=150">
-			</div>
-		</div>
-	</body>
-</html>
+    // Get latest photos according to #hashtag keyword
+    $media = $instagram->getTagMedia($tag);
+
+    // Set number of photos to show
+    $limit = 5;
+
+    // Set height and width for photos
+    $size = '100';
+
+    // Show results
+    // Using for loop will cause error if there are less photos than the limit
+    foreach(array_slice($media->data, 0, $limit) as $data)
+    {
+        // Show photo
+        echo '<p><img src="'.$data->images->thumbnail->url.'" height="'.$size.'" width="'.$size.'" alt="SOME TEXT HERE"></p>';
+    }
+?>
